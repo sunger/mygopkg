94902a8887d6dffe1d8c53fabf35b448869ea929
sunger <zhuyingqi5258@126.com>
2020-11-13 08:59:50 +0800

    修正权限判断之后还可以返回数据的bug

--- framework/gin_/AuthMiddleware.go
+++ framework/gin_/AuthMiddleware.go
@@ -1,45 +1,50 @@
 package gin_
 
 import (
+	"strings"
+
 	"github.com/gin-gonic/gin"
 	mycs "github.com/sunger/mygopkg/framework/casbin"
-	"strings"
 )
 
 //无域认证
-func AuthMiddleware()  Middleware {
+func AuthMiddleware() Middleware {
 	return func(next Endpoint) Endpoint {
 		return func(c *gin.Context, request interface{}) (response interface{}, err error) {
 			if c.Request.Header.Get("token") == "" {
 				c.AbortWithStatusJSON(400, gin.H{"message": "token required"})
+				return
 			}
 			token := c.Request.Header.Get("token")
 			user, err := ParseToken(token)
 			if err != nil {
 				c.AbortWithStatusJSON(403, gin.H{"message": err})
+				return
 			}
 			access, err := mycs.E.Enforce(user.ID, c.Request.RequestURI, c.Request.Method)
 			if err != nil || !access {
 				c.AbortWithStatusJSON(403, gin.H{"message": "forbidden"})
-			} else {
-				c.Next()
+				return
 			}
-			return next(c,request)
+			return next(c, request)
 		}
 	}
 }
+
 //有域认证
-func AuthWithDomainMiddleware()  Middleware {
+func AuthWithDomainMiddleware() Middleware {
 	return func(next Endpoint) Endpoint {
 		return func(c *gin.Context, request interface{}) (response interface{}, err error) {
 			if c.Request.Header.Get("token") == "" {
 				c.AbortWithStatusJSON(400, gin.H{"message": "token required"})
+				return
 			}
-			
+
 			token := c.Request.Header.Get("token")
 			user, err := ParseToken(token)
 			if err != nil {
 				c.AbortWithStatusJSON(403, gin.H{"message": err})
+				return
 			}
 
 			// user, _ := c.Get("id")
@@ -48,10 +53,9 @@ func AuthWithDomainMiddleware()  Middleware {
 			access, err := mycs.E.Enforce(user.ID, domain, uri, c.Request.Method)
 			if err != nil || !access {
 				c.AbortWithStatusJSON(403, gin.H{"message": "forbidden"})
-			} else {
-				c.Next()
+				return
 			}
-			return next(c,request)
+			return next(c, request)
 		}
 	}
 }
