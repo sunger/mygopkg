e4a4a6a982168fb099f7e83a3cfe21cbb98c5fb1
sunger <zhuyingqi5258@126.com>
2020-11-29 16:23:59 +0800

    upload

--- go.mod
+++ go.mod
@@ -9,11 +9,12 @@ require (
 	github.com/mojocn/base64Captcha v1.3.1
 	github.com/nfnt/resize v0.0.0-20180221191011-83c6a9932646
 	github.com/niemeyer/pretty v0.0.0-20200227124842-a10e7caefd8e // indirect
+	github.com/qiniu/api.v7/v7 v7.7.0
 	github.com/satori/go.uuid v1.2.0
 	github.com/spf13/viper v1.7.1
 	go.uber.org/zap v1.10.0
 	gopkg.in/check.v1 v1.0.0-20200902074654-038fdea0a05b // indirect
-	gopkg.in/natefinch/lumberjack.v2 v2.0.0
+	gopkg.in/natefinch/lumberjack.v2 v2.0.0 // indirect
 	gopkg.in/yaml.v2 v2.3.0 // indirect
 	gorm.io/driver/mysql v1.0.3
 	gorm.io/driver/sqlite v1.1.3
--- tools/image_test.go
+++ tools/image_test.go
@@ -1,11 +1,34 @@
 package tools
 
 import (
+	"flag"
+	"fmt"
+	"os"
 	"testing"
+
+	"github.com/sunger/mygopkg/config"
 )
 
 func TestBsModel(t *testing.T) {
 
 	imageUtil := ImageUtil{}
 	imageUtil.GetWH("")
+}
+
+func TestUpload(t *testing.T) {
+	env := flag.String("e", "development", "用法：xxx -e development|product|test")
+	flag.Usage = func() {
+		fmt.Println("Usage: server -e {mode}")
+		os.Exit(1)
+	}
+	flag.Parse()
+
+	// 初始化配置文件,这里非常重要，默认是development，理论上这里可以是任何值，只要存在类似：development。yaml文件
+	// 在微服务里可以针对每个模块启动对应的数据库连接，也就是不同的数据库
+	// 在本地单击部署支持不同的数据库，在云端可以考虑把这些配置文件存配置中心
+
+	config.Init(*env)
+	c := config.GetConfig()
+	defaultoss := c.GetString("oss.default")
+	fmt.Println(defaultoss)
 }
