a53074b809741f8d619f3d4b64c1ee7d34a7bc79
sunger <zhuyingqi5258@126.com>
2020-12-14 16:17:48 +0800

    日志加载路径修正到当前exe的运行目录（兼容运行在服务中）

--- config/config.go
+++ config/config.go
@@ -1,10 +1,11 @@
 package config
 
 import (
-	"log"
+	"os"
 	"path/filepath"
-
+	"github.com/sunger/mygopkg/log"
 	"github.com/spf13/viper"
+	"go.uber.org/zap"
 )
 
 var config *viper.Viper
@@ -14,13 +15,19 @@ var config *viper.Viper
 func Init(env string) {
 	var err error
 	v := viper.New()
+	path, err := os.Executable()
+	if err != nil {
+		log.GetLog().Error(err.Error())
+	}
+	dir := filepath.Dir(path)
+	v.AddConfigPath(filepath.Join(dir,"config"))
 	v.SetConfigType("yaml")
 	v.SetConfigName(env)
-	v.AddConfigPath("../config/")
-	v.AddConfigPath("config/")
+	//v.AddConfigPath("../config/")
+	//v.AddConfigPath("config/")
 	err = v.ReadInConfig()
 	if err != nil {
-		log.Fatal("error on parsing configuration file")
+		log.GetLog().Fatal("解析配置文件错误",zap.String("path",path))
 	}
 	config = v
 }
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.14
 require (
 	github.com/casbin/casbin/v2 v2.16.0
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/gin-contrib/cors v1.3.1
 	github.com/gin-gonic/gin v1.6.3
 	github.com/mojocn/base64Captcha v1.3.1
 	github.com/nfnt/resize v0.0.0-20180221191011-83c6a9932646
@@ -13,6 +14,8 @@ require (
 	github.com/satori/go.uuid v1.2.0
 	github.com/spf13/viper v1.7.1
 	go.uber.org/zap v1.10.0
+	golang.org/x/sys v0.0.0-20200930185726-fdedc70b468f // indirect
+	golang.org/x/text v0.3.3 // indirect
 	gopkg.in/check.v1 v1.0.0-20200902074654-038fdea0a05b // indirect
 	gopkg.in/natefinch/lumberjack.v2 v2.0.0
 	gopkg.in/yaml.v2 v2.3.0 // indirect
