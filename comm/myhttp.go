fe14eb07c7ce453d9958993172eb0d7b90e8db23
sunger <zhuyingqi5258@126.com>
2021-01-07 17:10:37 +0800

    修正http请求错误

--- comm/myhttp.go
+++ comm/myhttp.go
@@ -1,8 +1,6 @@
 package comm
 
 import (
-	"errors"
-	"fmt"
 	"io"
 	"io/ioutil"
 	"net/http"
@@ -14,7 +12,7 @@ func SendRequest(url string, body io.Reader, addHeaders map[string]string, metho
 	// 1、创建req
 	req, err := http.NewRequest(method, url, body)
 	if err != nil {
-		return
+		return nil, err
 	}
 	//req.Header.Add("Content-Type", "application/json")
 
@@ -28,18 +26,17 @@ func SendRequest(url string, body io.Reader, addHeaders map[string]string, metho
 	// 3、发送http请求
 	client := &http.Client{}
 	response, err := client.Do(req)
-	if err != nil {
-		return
-	}
 	defer response.Body.Close()
-
-	if response.StatusCode != 200 {
-		err = errors.New("http status err")
-		fmt.Errorf("sendRequest failed, url=%v, response status code=%d", url, response.StatusCode)
-		return
+	if err != nil {
+		return nil,err
 	}
+	//if response.StatusCode != 200 {
+	//	err = errors.New("http status err")
+	//	fmt.Errorf("sendRequest failed, url=%v, response status code=%d", url, response.StatusCode)
+	//	return nil
+	//}
 
 	// 4、结果读取
 	resp, err = ioutil.ReadAll(response.Body)
-	return
+	return resp, err
 }
