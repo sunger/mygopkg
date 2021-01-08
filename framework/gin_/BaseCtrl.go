a0122d132355ef0ed5ba87579d0aff3de3ce3d34
sunger <zhuyingqi5258@126.com>
2021-01-08 16:56:46 +0800

    加内存缓存

--- framework/gin_/BaseCtrl.go
+++ framework/gin_/BaseCtrl.go
@@ -5,6 +5,8 @@ import (
 	"github.com/sunger/mygopkg/log"
 )
 
+//防止重复解析，缓存token：用户信息
+var logins = make(map[string]UserInfo,0)
 // 控制器基类
 type BaseCtrl struct{}
 
@@ -16,12 +18,31 @@ func (m *BaseCtrl) User(c *gin.Context) UserInfo {
 		log.GetLog().Error("token为空")
 		return UserInfo{}
 	}
-	user, err := ParseToken(token)
-	if err != nil {
-		log.GetLog().Error("解析token错误")
-		return UserInfo{}
+
+	if v, ok := logins[token]; ok {
+		return v
+	} else {
+		user, err := ParseToken(token)
+		if err != nil {
+			log.GetLog().Error("解析token错误")
+			return UserInfo{}
+		}
+		//保存到缓存中
+		logins[token] = user
+		return user
 	}
-	return user
 }
 
+//清空
+func (m *BaseCtrl) Clear()  {
+	logins = make(map[string]UserInfo,0)
+}
+
+//重置
+func (m *BaseCtrl) Reset(oldtoken, newtoken string, user UserInfo)  {
+	if _, ok := logins[oldtoken]; ok {
+		delete(logins, oldtoken)
+	}
+	logins[newtoken] = user
+}
 
