be37309f532e7459ff008101d660282b52cfa326
sunger <zhuyingqi5258@126.com>
2021-02-24 21:39:24 +0800

    fix json

--- db/settings.go
+++ db/settings.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	"fmt"
+
 	"github.com/sunger/mygopkg/log"
 	"github.com/sunger/mygopkg/model"
 	"go.uber.org/zap"
@@ -11,20 +12,19 @@ import (
 type SettingCate struct {
 
 	//模块代码
-	ModuleCode string `gorm:"column:mcode;type:varchar(50);"`
+	ModuleCode string `gorm:"column:mcode;type:varchar(50);" json:"ModuleCode"`
 	//名称
-	Name string `gorm:"column:name;type:varchar(50);"`
+	Name string `gorm:"column:name;type:varchar(50);" json:"Name"`
 	//显示排序
-	Idx int `gorm:"column:idx;"`
+	Idx int `gorm:"column:idx;" json:"Idx"`
 	model.BsModel
 }
+
 // 键值配置分类表
 type SettingCateInstall struct {
 	SettingCate
 	Items []Settings
 }
-
-
 
 func (SettingCate) TableName() string {
 	return "s_settingcate"
@@ -35,30 +35,30 @@ func (r *SettingCate) List() (results []SettingCate, err error) {
 	err = Db.Find(&results).Error
 	return results, err
 }
+
 //删除配置分类，先删除配置项
 func (apps *SettingCate) Del(id string) (err error) {
 	Db.Where("cate = ?", id).Delete(Settings{})
 	return Db.Where("id = ?", id).Delete(SettingCate{}).Error
 }
 func (u *SettingCate) Insert() (id string) {
-	if u.Id=="" || len(u.Id) == 0{
+	if u.Id == "" || len(u.Id) == 0 {
 		u.CreateId()
 	}
 	Db.Create(&u)
 	return u.Id
 }
 
-
 // 键值配置表
 type Settings struct {
 	//分类id
-	Cate string `gorm:"column:cate;type:varchar(50);"`
+	Cate string `gorm:"column:cate;type:varchar(50);" json:"Cate"`
 	//控件json
-	Ctrl string `gorm:"column:ctrl;type:varchar(500);"`
+	Ctrl string `gorm:"column:ctrl;type:varchar(500);" json:"Ctrl"`
 	//Value
-	Val string `gorm:"column:v;type:varchar(250);"`
+	Val string `gorm:"column:v;type:varchar(250);" json:"Val"`
 	//显示排序
-	Idx int `gorm:"column:idx;"`
+	Idx int `gorm:"column:idx;" json:"Idx"`
 	model.BModel
 }
 
@@ -67,7 +67,7 @@ func (Settings) TableName() string {
 }
 
 func (u *Settings) Insert() (id string) {
-	if u.Id=="" || len(u.Id) == 0{
+	if u.Id == "" || len(u.Id) == 0 {
 		u.CreateId()
 	}
 	Db.Create(&u)
@@ -82,13 +82,13 @@ func (apps *Settings) Del(id string) (err error) {
 func (apps *Settings) Update() (err error) {
 	a := Settings{}
 	//a.Id = apps.Id
-	count  := Db.Where("id=?", apps.Id).Find(&a).RowsAffected
+	count := Db.Where("id=?", apps.Id).Find(&a).RowsAffected
 	if count == 0 {
-		log.GetLog().Info("不存在"+apps.Id)
+		log.GetLog().Info("不存在" + apps.Id)
 		Db.Create(&apps)
-		return  err
-	}else{
-		log.GetLog().Info("存在"+apps.Id)
+		return err
+	} else {
+		log.GetLog().Info("存在" + apps.Id)
 		return Db.Model(&a).Updates(map[string]interface{}{"v": apps.Val}).Error
 	}
 }
@@ -101,9 +101,10 @@ func (apps *Settings) Get(id string) (Settings, error) {
 	return a, err
 }
 func (r *Settings) List(code string) (results []Settings, err error) {
-	err = Db.Where("cate=?",code).Find(&results).Error
+	err = Db.Where("cate=?", code).Find(&results).Error
 	return results, err
 }
+
 //分页方法
 func (b *Settings) PageList(page, size int, filter string, sort string) ([]Settings, int) {
 	db := Db
