ebccf142a239a36670dcd2c06d0e80d44483f65e
runter <runter@tom.com>
2021-06-18 13:32:48 +0800

    token添加过期时间的方法

--- framework/gin_/MyJwt.go
+++ framework/gin_/MyJwt.go
@@ -33,6 +33,26 @@ func CreateToken(user *UserInfo) (tokenss string, err error) {
 	return
 }
 
+func CreateTokenWithEpr(user *UserInfo,exp int64) (tokenss string, err error) {
+	//自定义claim
+	claim := jwt.MapClaims{
+		"id": user.ID,
+		"did": user.Db,
+		"exp":	exp,
+		"nbf": time.Now().Unix(),
+		"iat": time.Now().Unix(),
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claim)
+	// 签名后的token格式说明
+	// 示例：eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE1NzUyODUyNzUsImlkIjoxLCJuYmYiOjE1NzUyODUyNzUsInVzZXJuYW1lIjoicGVuZ2oifQ.bDe8UZYLxvmrK7gHcuK8TrlnoiMsIm3Jo_f0-YYle7E
+	// 使用符号.，被分割成了三段
+	// 第一段base64解码之后：{"alg":"HS256","typ":"JWT"}
+	// 第二段base64解码之后：{"iat":1575285275,"id":1,"nbf":1575285275,"username":"pengj"}，是原始的数据。
+	// 第三段是使用SigningMethodHS256加密之后的文本
+	tokenss, err = token.SignedString([]byte(SecrectStr))
+	return
+}
+
 func secret() jwt.Keyfunc {
 	return func(token *jwt.Token) (i interface{}, e error) {
 		return []byte(SecrectStr), nil
